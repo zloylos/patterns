badb5d8934cb6e657c7f8edb41e9587779a1a192
Denis Hananein <i@zloylos.me>
2013-07-14 19:16:30 +0400

    start

---
+++ slide_config.js
@@ -0,0 +1,27 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Паттерны проектирования<br>и их реализация на JavaScript',
+    subtitle: '',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+/*     favIcon: 'images/google_developers_logo_tiny.png', */
+    fonts: [
+      'PT Sans:regular,semibold,italic,italicsemibold',
+      'PT Mono'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Денис Хананеин',
+    company: 'Разработчик интерфейсов<br>группа разработки JS API <span style="color:red">Я</span>ндекс.Карт',
+    twitter: '@kandasoft',
+    www: 'http://zloylos.me',
+    github: 'http://github.com/zloylos'
+  }]
+};
+
